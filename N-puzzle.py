fba6cbf62e6e17be713f5f6db1d7e0ab92363ea6
afokin <andrii.fokin@ubisoft.com>
2019-10-27 17:11:45 +0200

    parser

--- N-puzzle.py
+++ N-puzzle.py
@@ -27,6 +27,44 @@ class Puzzle:
         return [index % self.size, index // self.size]
 
 
+def format_line(line):
+    line = line[:line.find('#')]
+    return line
+
+def get_formated_lines(lines_arr):
+    return [format_line(line) for line in lines_arr]
+
+
+def get_puzzle_data_lines_from_file(file: str) -> str:
+    f = open(file,"r+")
+    
+    lines = [line for line in get_formated_lines(f.readlines()) if len(line) > 0]
+    lines_len = len(lines)
+
+    assert(lines_len > 1 or int(lines[0]) + 1 != lines_len , "Wrong number of lines")
+    # Puzzle format
+    # 3
+    # 6 8 0
+    # 2 7 4
+    # 3 1 5
+
+    return lines
+
+
+
+def get_puzzle_from_file(file: str)-> Puzzle:
+
+    puzzle_lines = get_puzzle_data_lines_from_file(file)
+    puzzle_size = int(puzzle_lines[0])
+
+    puzzle_arr = []
+    for line in puzzle_lines[1:]:
+        puzzle_row = [int(number) for number in line.split(' ')]
+        assert(len(puzzle_row) != puzzle_size, f'Wrong number of values in a row: {puzzle_row}')
+        puzzle_arr.append(*puzzle_row)
+
+    return Puzzle(puzzle_arr)
+
 
 if __name__ == "__main__":
 
@@ -40,4 +78,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print(f'file: {args.file}')
+    print (get_puzzle_from_file(args.file))
 
